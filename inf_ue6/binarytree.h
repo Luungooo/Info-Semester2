fa89d6caaae23b813518461cdeeda8bf2184364f
Luungooo <lungoo.dev@protonmail.com>
2022-05-05 20:34:37 +0200

    Added ue6

---
+++ inf_ue6/binarytree.h
@@ -0,0 +1,8 @@
+#ifndef INF_UE6_BINARYTREE_H
+#define INF_UE6_BINARYTREE_H
+typedef struct binaryTree {
+    char value;
+    struct binaryTree* left;
+    struct binaryTree* right;
+}bt;
+#endif //INF_UE6_BINARYTREE_H
